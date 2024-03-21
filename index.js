f006540794187a4b1f1a77e084b2f6a3bc4c9b50
+ <+>
2024-03-21 20:03:51 +0300

    creates loop in js file

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let messages = []; 
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;  
+        messages.push(message); 
+    }
+return messages;
+}
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
